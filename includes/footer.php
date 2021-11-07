307c31b0b790e498179a8ed1a653b6fba26c8787
Nimtara Munasinghe <73902228+nimtaraMunasinghe@users.noreply.github.com>
2021-11-07 18:19:03 +0530

    update footer bottom

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,12 @@
+
+<!-- footer  starts-->
+
 <footer>
     <div class="footer-container">
         <div class="footer-upper">
+
+            <!-- about section in footer -->
+
             <section class="footer-sec about">
                 <div class="footer-head">
                     <h3>About</h3>
@@ -14,6 +20,9 @@
                     </ul>
                 </div>
             </section>
+
+            <!-- community section in footer -->
+
             <section class="footer-sec community">
                 <div class="footer-head">
                     <h3>Community</h3>
@@ -27,6 +36,9 @@
                     </ul>
                 </div>
             </section>
+
+            <!-- host section in footer -->
+
             <section class="footer-sec host">
                 <div class="footer-head">
                     <h3>Host</h3>
@@ -40,6 +52,9 @@
                     </ul>
                 </div>
             </section>
+
+            <!-- support section in footer -->
+
             <section class="footer-sec support">
                 <div class="footer-head">
                     <h3>Support</h3>
@@ -54,8 +69,35 @@
                 </div>
             </section>
         </div>
+
+       <div class="footer-hr">
+           <hr>
+       </div>
+
+        <!-- footer bottom -->
+
         <div class="footer-bottom">
+            <div class="footer-bottom-links">
+                <div class="footer-bottom-links left">
+                    <ul>
+                        <li>&#169; Airbnb, Inc.</li>
+                        <li><a href="#">Privacy</a></li>
+                        <li><a href="#">Terms</a></li>
+                        <li><a href="#">Sitemap</a></li>
+                    </ul>
+                </div>
+                <div class="footer-bottom-links right">
+                    <ul>
+                        <li><a href="#">Sitemap</a></li>
+                        <li><a href="#">Sitemap</a></li>
+                        <li><a href="#">Sitemap</a></li>
+                        <li><a href="#">Sitemap</a></li>
+                    </ul>
+                </div>
+            </div>
 
         </div>
     </div>
 </footer>
+
+<!-- footer ends -->
