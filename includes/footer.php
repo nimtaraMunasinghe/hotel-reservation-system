68f224836e342bf7ac5a6e367d0f192f5d007ea9
Nimtara Munasinghe <73902228+nimtaraMunasinghe@users.noreply.github.com>
2021-11-13 21:37:58 +0530

    update footer

--- includes/footer.php
+++ includes/footer.php
@@ -9,14 +9,15 @@
 
             <section class="footer-sec about">
                 <div class="footer-head">
-                    <h3>About</h3>
+                    <h4>Support</h4>
                 </div>
                 <div class="footer-links">
                     <ul>
-                        <li><a href="#">Link</a></li>
-                        <li><a href="#">Link</a></li>
-                        <li><a href="#">Link</a></li>
-                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Help Center</a></li>
+                        <li><a href="#">Safety information</a></li>
+                        <li><a href="#">Cancellation options</a></li>
+                        <li><a href="#">Supporting people with disabilities</a></li>
+                        <li><a href="#">Report a neighborhood concern</a></li>
                     </ul>
                 </div>
             </section>
@@ -25,14 +26,14 @@
 
             <section class="footer-sec community">
                 <div class="footer-head">
-                    <h3>Community</h3>
+                    <h4>Community</h4>
                 </div>
                 <div class="footer-links">
                     <ul>
-                        <li><a href="#">Link</a></li>
-                        <li><a href="#">Link</a></li>
-                        <li><a href="#">Link</a></li>
-                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Airbnb.org: disaster relief housing</a></li>
+                        <li><a href="#">Support Afghan refugees</a></li>
+                        <li><a href="#">Celebrating diversity & belonging</a></li>
+                        <li><a href="#">Combating discrimination</a></li>
                     </ul>
                 </div>
             </section>
@@ -41,14 +42,15 @@
 
             <section class="footer-sec host">
                 <div class="footer-head">
-                    <h3>Host</h3>
+                    <h4>Hosting</h4>
                 </div>
                 <div class="footer-links">
                     <ul>
-                        <li><a href="#">Link</a></li>
-                        <li><a href="#">Link</a></li>
-                        <li><a href="#">Link</a></li>
-                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Try hosting</a></li>
+                        <li><a href="#">AirCover: protection for Hosts</a></li>
+                        <li><a href="#">Explore hosting resources</a></li>
+                        <li><a href="#">Visit our community forum</a></li>
+                        <li><a href="#">How to host responsibly</a></li>
                     </ul>
                 </div>
             </section>
@@ -57,14 +59,16 @@
 
             <section class="footer-sec support">
                 <div class="footer-head">
-                    <h3>Support</h3>
+                    <h4>About</h4>
                 </div>
                 <div class="footer-links">
                     <ul>
-                        <li><a href="#">Link</a></li>
-                        <li><a href="#">Link</a></li>
-                        <li><a href="#">Link</a></li>
-                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Newsroom</a></li>
+                        <li><a href="#">Learn about new features</a></li>
+                        <li><a href="#">Letter from our founders</a></li>
+                        <li><a href="#">Careers</a></li>
+                        <li><a href="#">Investors</a></li>
+                        <li><a href="#">Airbnb Luxe</a></li>
                     </ul>
                 </div>
             </section>
@@ -88,10 +92,11 @@
                 </div>
                 <div class="footer-bottom-links right">
                     <ul>
-                        <li><a href="#">Sitemap</a></li>
-                        <li><a href="#">Sitemap</a></li>
-                        <li><a href="#">Sitemap</a></li>
-                        <li><a href="#">Sitemap</a></li>
+                        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                        <li><a href="#"><i class="fas fa-dollar-sign"></i>  USD</a></li>
+                        <li><a href="#"><i class="fas fa-globe"></i>  English (US)</a></li>
                     </ul>
                 </div>
             </div>
